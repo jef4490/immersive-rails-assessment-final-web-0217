a010fa11c7abe5d3adbcb150cff36914b2a86a19
jef4490 <jeffrey.hechler@gmail.com>
2017-03-27 14:06:45 -0400

    core featureset

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -4,5 +4,8 @@ class Appearance < ApplicationRecord
   belongs_to :user
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { :in => 1..5, message: "The rating must be between 1 and 5."}
+  validates :user_id, presence: true
+  validates :guest_id, presence: true
+  validates :episode_id, presence: true
 
 end
