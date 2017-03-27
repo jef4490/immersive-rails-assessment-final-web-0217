5a6cfbac9f76eb77e15aa984189968cb49d186e7
jef4490 <jeffrey.hechler@gmail.com>
2017-03-27 14:19:09 -0400

    logout system + root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def logged_in?
-    !!session[:account_id]
+    !!session[:user_id]
   end
 
   def current_user
-    @account ||= Account.find_by_id(session[:account_id])
+    @user ||= User.find_by_id(session[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "root#index"
+
   resources :guests, only: [:index, :show]
   resources :episodes, only: [:index, :show]
   resources :appearances, only: [:new, :create, :edit, :update]
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/sessions', to: 'sessions#create', as: 'sessions'
-
+  delete '/sessions', to: 'sessions#destroy', as: 'logout'
+  
 end
